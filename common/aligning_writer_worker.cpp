0ce801ce3f5e687ee1aae4427a354f73340dc167
Paweł Skórzewski <pawels@uam5050>
2013-03-27 15:26:22 +0100

    Add protected print_ etc., make alignOutput_ etc. private

--- common/aligning_writer_worker.cpp
+++ common/aligning_writer_worker.cpp
@@ -7,6 +7,51 @@ AligningWriterWorker::AligningWriterWorker(std::ostream& outputStream, Lattice& 
 AligningWriterWorker::~AligningWriterWorker() {
 }
 
+void AligningWriterWorker::setAlignments_(std::vector<unsigned int> alignments) {
+    alignments_ = alignments;
+}
+
+void AligningWriterWorker::print_(std::string text) {
+    alignOutput_(text);
+}
+
+void AligningWriterWorker::printLine_(std::string text) {
+    alignOutput_(text);
+    alignOutputNewline_();
+}
+
+void AligningWriterWorker::printLine_() {
+    alignOutputNewline_();
+}
+
+void AligningWriterWorker::printTableRow_(std::vector<std::string> cellsContents) {
+    if (!cellsContents.empty()) {
+        for (size_t i = 0; i < cellsContents.size() - 1; ++i) {
+            alignOutput_(cellsContents[i], getAlignment_(i));
+            alignOutput_(" ");
+        }
+        alignOutput_(cellsContents.back());
+    }
+    alignOutputNewline_();
+}
+
+unsigned int AligningWriterWorker::getColumnWidth_(size_t columnNum) {
+    return getAlignment_(columnNum) - ((columnNum > 0) ? getAlignment_(columnNum - 1) : 0);
+}
+
+const unsigned int AligningWriterWorker::DEFAULT_COLUMN_WIDTH = 20;
+
+unsigned int AligningWriterWorker::getAlignment_(size_t columnNum) {
+    if (columnNum < alignments_.size()) {
+        return alignments_[columnNum];
+    }
+    unsigned int lastAlignment = 0;
+    if (!alignments_.empty()) {
+        lastAlignment = alignments_.back();
+    }
+    return lastAlignment + DEFAULT_COLUMN_WIDTH * (columnNum - alignments_.size());
+}
+
 unsigned int AligningWriterWorker::alignOutput_(std::string output) {
     getOutputStream() << output;
     currentPos_ += utf8::distance(output.begin(), output.end());
@@ -29,35 +74,3 @@ unsigned int AligningWriterWorker::alignOutputNewline_() {
     currentPos_ = 0;
     return currentPos_;
 }
-
-void AligningWriterWorker::setAlignments_(std::vector<unsigned int> alignments) {
-    alignments_ = alignments;
-}
-
-void AligningWriterWorker::printTableRow_(std::vector<std::string> cellsContents) {
-    if (!cellsContents.empty()) {
-        for (size_t i = 0; i < cellsContents.size() - 1; ++i) {
-            alignOutput_(cellsContents[i], getAlignment_(i));
-            alignOutput_(" ");
-        }
-        alignOutput_(cellsContents.back());
-    }
-    alignOutputNewline_();
-}
-
-unsigned int AligningWriterWorker::getColumnWidth_(size_t columnNum) {
-    return getAlignment_(columnNum) - ((columnNum > 0) ? getAlignment_(columnNum - 1) : 0);
-}
-
-unsigned int AligningWriterWorker::getAlignment_(size_t columnNum) {
-    if (columnNum < alignments_.size()) {
-        return alignments_[columnNum];
-    }
-    unsigned int lastAlignment = 0;
-    if (!alignments_.empty()) {
-        lastAlignment = alignments_.back();
-    }
-    return lastAlignment + DEFAULT_COLUMN_WIDTH * (columnNum - alignments_.size());
-}
-
-const unsigned int AligningWriterWorker::DEFAULT_COLUMN_WIDTH = 20;
