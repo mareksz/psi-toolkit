1eb77faa0bfa96c9308a8664030956968b995fb6
Roman Grundkiewicz <romang@amu.edu.pl>
2013-03-29 15:39:19 +0100

    update shallow aliaser

--- framework/shallow_aliaser.cpp
+++ framework/shallow_aliaser.cpp
@@ -8,43 +8,9 @@
 
 typedef std::map<std::string, std::string> string_map;
 
-const std::map<std::string, std::string> ShallowAliaser::aliases_ =
-    boost::assign::map_list_of
-        ("write-tokens",    "simple-writer --tag token")
-        ("get-tokens",      "simple-writer --tag token")
-        ("write-segments",  "simple-writer --tag segment")
-        ("get-segments",    "simple-writer --tag segment")
-
-        ("read-docx",       "apertium-reader --format docx")
-        ("docx-reader",     "apertium-reader --format docx")
-        ("read-xlsx",       "apertium-reader --format xlsx")
-        ("xlsx-reader",     "apertium-reader --format xlsx")
-        ("read-pptx",       "apertium-reader --format pptx")
-        ("pptx-reader",     "apertium-reader --format pptx")
-        ("read-html",       "apertium-reader --format html")
-        ("html-reader",     "apertium-reader --format html")
-
-        //("write-tokens-and-segments",
-        //    "write --tag symbol --sep '' --spec token ' ' segment \\n")
-        //("get-tokens-and-segments",
-        //    "write --tag symbol --sep '' --spec token ' ' segment \\n")
-
-        ("tree",            "gv-writer --tree")
-        ("write-tree",      "gv-writer --tree")
-        ("draw-tree",       "gv-writer --tree")
-
-        ("parse-tree",          "gv-writer --tree --filter parse")
-        ("write-parse-tree",    "gv-writer --tree --filter parse")
-        ("draw-parse-tree",     "gv-writer --tree --filter parse")
-
-        ("write-xml",   "bracketing-writer --skip-symbol-edges --opening-bracket \n"
-            "<edge tags=\"%T\" category=\"%c\" attrs=\"%A\"> --closing-bracket </edge>\n"
-            " --av-separator : --av-pairs-separator ;")
-        ("xml-writer",  "bracketing-writer --skip-symbol-edges --opening-bracket \n"
-            "<edge tags=\"%T\" category=\"%c\" attrs=\"%A\"> --closing-bracket </edge>\n"
-            " --av-separator : --av-pairs-separator ;")
-        ;
-
+ShallowAliaser::ShallowAliaser() {
+    initializeAliases_();
+}
 
 std::string ShallowAliaser::replace(const std::string& pipeline) {
     std::string result = std::string(" " + pipeline + " ");
@@ -69,47 +35,43 @@ std::vector<std::string> ShallowAliaser::replace(std::vector<std::string> pipeli
     return result;
 }
 
-/*
 void ShallowAliaser::initializeAliases_() {
-    addWriteGetAlias("tokens",   "simple-writer --tag token");
-    addWriteGetAlias("segments", "simple-writer --tag segment");
-
-    addWriteGetAlias("tokens-and-segments",
-        "write --tag symbol --sep '' --spec token ' ' segment \\n");
-    addWriteGetAlias("tokens-with-segments",
-        "write --tag symbol --sep '' --spec token ' ' segment \\n");
-
-    addWriteGetAlias("xml", "bracketing-writer --skip-symbol-edges --opening-bracket \n"
-        "<edge tags=\"%T\" category=\"%c\" attrs=\"%A\"> --closing-bracket </edge>\n"
-        " --av-separator : --av-pairs-separator ;");
-
     addReadReaderAlias("docx", "apertium-reader --format docx");
     addReadReaderAlias("xlsx", "apertium-reader --format xlsx");
     addReadReaderAlias("pptx", "apertium-reader --format pptx");
     addReadReaderAlias("html", "apertium-reader --format html");
 
-    addNullDrawWriteAlias("tree", "gv-writer --tree");
-    addNullDrawWriteAlias("parse-tree", "gv-writer --tree --filter-parse");
-}
+    addWriteGetAlias("tokens",   "simple-writer --tag token");
+    addWriteGetAlias("segments", "simple-writer --tag segment");
 
-void ShallowAliaser::addWriteGetAlias(std::string alias, std::string expand) {
-    aliases_["write-" + alias] = expand;
-    aliases_["get-" + alias] = expand;
+    addWriteWriterGetAlias("xml", "bracketing-writer --skip-symbol-edges --opening-bracket \n"
+        "<edge tags=\"%T\" category=\"%c\" attrs=\"%A\"> --closing-bracket </edge>\n"
+        " --av-separator : --av-pairs-separator ;");
+
+    addWriteGetDrawAlias("tree", "gv-writer --tree");
+    addWriteGetDrawAlias("parse-tree", "gv-writer --tree --filter-parse");
 }
 
 void ShallowAliaser::addReadReaderAlias(std::string alias, std::string expand) {
-    aliases_["read-" + alias] = expand;
-    aliases_[alias + "-reader"] = expand;
+    addAlias_("read-" + alias, expand);
+    addAlias_(alias + "-reader", expand);
+}
+
+void ShallowAliaser::addWriteGetAlias(std::string alias, std::string expand) {
+    addAlias_("write-" + alias, expand);
+    addAlias_("get-" + alias, expand);
 }
 
 void ShallowAliaser::addWriteWriterGetAlias(std::string alias, std::string expand) {
     addWriteGetAlias(alias, expand);
-    aliases_[alias + "-writer"] = expand;
+    addAlias_(alias + "-writer", expand);
 }
 
-void ShallowAliaser::addNullDrawWriteAlias(std::string alias, std::string expand) {
-    aliases_[alias] = expand;
-    aliases_["draw-" + alias] = expand;
-    aliases_["write-" + alias] = expand;
+void ShallowAliaser::addWriteGetDrawAlias(std::string alias, std::string expand) {
+    addWriteGetAlias(alias, expand);
+    addAlias_("draw-" + alias, expand);
 }
-*/
+
+void ShallowAliaser::addAlias_(std::string key, std::string value) {
+    aliases_.insert(std::pair<std::string, std::string>(" " + key + " ", " " + value + " "));
+}
--- framework/shallow_aliaser.hpp
+++ framework/shallow_aliaser.hpp
@@ -9,20 +9,22 @@ class ShallowAliaser {
 
 public:
 
+    ShallowAliaser();
+
     std::string replace(const std::string& pipeline);
     std::vector<std::string> replace(std::vector<std::string> pipeline);
 
-private:
-
-    static const std::map<std::string, std::string> aliases_;
-
-    /*
-    void initializeAliases_();
-    void addWriteGetAlias(std::string, std::string);
     void addReadReaderAlias(std::string, std::string);
     void addWriteWriterGetAlias(std::string, std::string);
-    void addNullDrawWriteAlias(std::string, std::string);
-    */
+    void addWriteGetAlias(std::string, std::string);
+    void addWriteGetDrawAlias(std::string, std::string);
+
+private:
+
+    std::map<std::string, std::string> aliases_;
+
+    void addAlias_(std::string, std::string);
+    void initializeAliases_();
 };
 
 #endif
