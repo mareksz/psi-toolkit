ca34d1530bb608a79315a6bce63856fa9e2d82a6
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2013-04-25 15:31:39 +0200

    Split pipeline with boost::tokenizer instead of boost::split

--- framework/split_pipeline.cpp
+++ framework/split_pipeline.cpp
@@ -1,10 +1,15 @@
 #include "split_pipeline.hpp"
 
-#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+#include <boost/tokenizer.hpp>
 
 std::vector<std::string> splitPipeline(const std::string& pipeline) {
     std::vector<std::string> strs;
     // the first element should be the name of the program
-    boost::split(strs, pipeline, boost::is_any_of(" "));
+    boost::tokenizer< boost::escaped_list_separator<char> >
+        tokenized(pipeline, boost::escaped_list_separator<char>('\\', ' ', '\"'));
+    BOOST_FOREACH(std::string token, tokenized) {
+        strs.push_back(token);
+    }
     return strs;
 }
