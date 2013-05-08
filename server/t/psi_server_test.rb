6e11b6fa6fc4c1f14f57553bd582922f6b46fc27
Roman Grundkiewicz <romang@amu.edu.pl>
2013-05-08 15:32:41 +0200

    add psi-server unit tests

--- server/t/psi_server_test.rb
+++ server/t/psi_server_test.rb
@@ -6,48 +6,67 @@ require 'config'
 
 class PsiServerTest < Test::Unit::TestCase
 
+  MIN_NUMBER_OF_ALIASES = 25
+
+  def setup
+    @browser = Celerity::Browser.new
+  end
+
   def test_server_address
-      assert_nothing_raised(NativeException) do
-      browser = Celerity::Browser.new
-      browser.goto $server_address
-      browser.close
+    assert_nothing_raised(NativeException) do
+      @browser.goto $server_address
+      @browser.close
     end
   end
 
-  def test_if_list_of_publications_exists
-    browser = Celerity::Browser.new
+  def test_list_of_publications_exists
     assert_nothing_raised(NativeException) do
-      browser.goto $server_address + '/publications.html'
+      @browser.goto $server_address + '/publications.html'
     end
 
-    assert browser.ul(:id => 'publications').exists?
-
-    browser.close
+    assert @browser.ul(:id => 'publications').exists?
   end
 
-  def test_if_download_site_works
-    browser = Celerity::Browser.new
+  def test_list_of_aliases_works
     assert_nothing_raised(NativeException) do
-      browser.goto $server_address + '/download.html'
+      @browser.goto $server_address + '/help/aliases.html'
     end
 
-    browser.div(:id => 'downloads').uls.each do |ul|
+    all_text = @browser.div(:id => 'content').text.downcase
+    assert all_text.include? 'aliases'
+    assert all_text.include? 'lemmatize'
+    assert all_text.include? 'tokenize'
+
+    number_of_aliases = @browser.divs.count{ |div| div.attribute_value(:class) == 'alias-item' }
+    assert number_of_aliases > MIN_NUMBER_OF_ALIASES
+  end
+
+  def test_download_site_works
+    assert_nothing_raised(NativeException) do
+      @browser.goto $server_address + '/download.html'
+    end
+
+    @browser.div(:id => 'downloads').uls.each do |ul|
       ul.as.each { |link| assert link.href.start_with?('http://mrt.wmi.amu.edu.pl:8080/job') }
     end
-
-    browser.close
   end
 
-  def test_if_bindings_site_works
-    browser = Celerity::Browser.new
+  def test_bindings_site_works
     assert_nothing_raised(NativeException) do
-      browser.goto $server_address + '/bindings.html'
+      @browser.goto $server_address + '/bindings.html'
     end
 
-    assert browser.h2s.map(&:text).join(' ').include? 'Perl'
-    assert browser.h2s.map(&:text).join(' ').include? 'Python'
+    assert @browser.h2s.map(&:text).join(' ').include? 'Perl'
+    assert @browser.h2s.map(&:text).join(' ').include? 'Python'
+    assert @browser.h2s.map(&:text).join(' ').include? 'Ruby'
+  end
 
-    browser.close
+  def test_email_presence_on_contact_site
+    assert_nothing_raised(NativeException) do
+      @browser.goto $server_address + '/contact.html'
+    end
+
+    assert @browser.div(:id => 'content').text.include? 'psi-toolkit@amu.edu.pl'
   end
 
 end
