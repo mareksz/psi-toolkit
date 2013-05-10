acfd66c65bce10fe0d6824b3377df64072113f2c
Roman Grundkiewicz <romang@amu.edu.pl>
2013-05-10 11:07:52 +0200

    add tests for tutorial page

--- server/t/help_site_test.rb
+++ server/t/help_site_test.rb
@@ -6,48 +6,50 @@ require 'config'
 
 class HelpSiteTest < Test::Unit::TestCase
 
-    def setup
-      @browser = Celerity::Browser.new
-      @browser.goto $server_address + '/help/documentation.html'
-    end
+  def setup
+    @browser = Celerity::Browser.new
+    @browser.goto $server_address + '/help/documentation.html'
+  end
 
-    def teardown
-      @browser.close
-    end
+  def teardown
+    @browser.close
+  end
 
-    def test_help_container_exists
-      assert @browser.div(:class, 'help-item').exists?
-    end
+  def test_help_container_exists
+    assert @browser.div(:class, 'help-item').exists?
+  end
 
-    def test_each_processor_has_title_and_description
-      titles = @browser.div(:id => 'content').h2s
-      assert !titles.empty?
+  def test_each_processor_has_title_and_description
+    titles = @browser.div(:id => 'content').h2s
+    assert !titles.empty?
 
-      descriptions = divs_with_class('help-desc')
-      assert !descriptions.empty?
+    descriptions = divs_with_class('help-desc')
+    assert !descriptions.empty?
 
-      assert_equal titles.size, descriptions.size
-    end
+    assert_equal titles.size, descriptions.size
+  end
 
-    def test_some_processors_have_option_list
-      options = divs_with_class('help-opts')
-      assert !options.empty?
-    end
+  def test_some_processors_have_option_list
+    options = divs_with_class('help-opts')
+    assert !options.empty?
+  end
 
-    def test_some_processors_have_language_list
-      langs = divs_with_class('help-langs')
-      assert !langs.empty?
-    end
+  def test_some_processors_have_language_list
+    langs = divs_with_class('help-langs')
+    assert !langs.empty?
+  end
 
-    def test_some_processors_have_aliases
-      aliases = divs_with_class('help-alias')
-      assert !aliases.empty?
-    end
+  def test_some_processors_have_aliases
+    aliases = divs_with_class('help-alias')
+    assert !aliases.empty?
+  end
 
-    private
 
-    def divs_with_class(class_name)
-      @browser.divs.select{ |div| div.attribute_value(:class) == class_name }
-    end
+  private
+
+  def divs_with_class(class_name)
+    @browser.divs.select{ |div| div.attribute_value(:class) == class_name }
+  end
+
 
 end
---
+++ server/t/tutorial_site_test.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'celerity'
+require 'test/unit'
+
+require 'config'
+
+class TutorialSiteTest < Test::Unit::TestCase
+
+  def setup
+    @browser = Celerity::Browser.new
+    @browser.goto $server_address + '/help/tutorial.html'
+  end
+
+  def teardown
+    @browser.close
+  end
+
+  def test_all_links_in_tutorial_works
+    links = []
+    @browser.div(:id => 'content').ps.each do |p|
+      links += p.links.select{ |link| !link_with_runable_example?(link) }
+    end
+    links.compact!
+
+    assert !links.empty?
+
+    links.each do |link|
+      assert_not_equal link.click_and_attach.status_code, 404
+    end
+  end
+
+  def test_links_with_runable_examples_in_tutorial_works
+    links = []
+    @browser.div(:id => 'content').ps.each do |p|
+      links += p.links.select{ |link| link_with_runable_example?(link) }
+    end
+    links.compact!
+
+    assert !links.empty?
+
+    links.each do |link|
+      psi = link.click_and_attach
+      assert_not_equal psi.status_code, 404
+      assert !psi.text.downcase.include?('there are some problems')
+    end
+  end
+
+  private
+
+  def link_with_runable_example?(link)
+    link.href.to_s.include?('pipe.psis?')
+  end
+
+end
