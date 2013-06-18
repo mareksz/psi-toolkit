81a4644c32f35f90fbbd904370abe8ba8299b39b
Filip Gralinski <filipg@amu.edu.pl>
2013-06-18 23:49:02 +0200

    static compilation is possible now

--- static.sh
+++ static.sh
@@ -4,5 +4,5 @@ find . -name CMakeLists.txt -exec perl -i -pne 's/ SHARED/ STATIC/' '{}' ';'
 rm -rf build
 mkdir build
 cd build
-cmake .. -DUSE_SFST=OFF -DUSE_GV_WRITER=OFF -DUSE_LINK_GRAMMAR=OFF -DUSE_ASPELL=OFF -DUSE_PDF_READER=OFF
+cmake .. -DCMAKE_BUILD_TYPE=Coverage -DUSE_SFST=OFF -DUSE_GV_WRITER=OFF -DUSE_LINK_GRAMMAR=OFF -DUSE_ASPELL=OFF -DUSE_PDF_READER=OFF
 make -j2
