4a32428331e06e56dc49597434bffe6a5bb65619
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2013-10-30 18:50:59 +0100

    Update Gobio limits

--- tools/parsers/gobio/chart/gobio_limit_checker.cpp
+++ tools/parsers/gobio/chart/gobio_limit_checker.cpp
@@ -5,7 +5,9 @@ GobioLimitChecker::GobioLimitChecker(std::string lang) {
     if (lang == "pl") {
         setAbsoluteLimit(10);
         addUnlimitedSpec(30);
-        addLimitSpec(50, 80);
+        addLimitSpec(40, 200);
+        addLimitSpec(50, 140);
+        addLimitSpec(70, 80);
         addLimitSpec(100, 15);
     }
 }
