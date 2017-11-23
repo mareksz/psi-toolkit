8853c3902d80232492c789146dbe2f8b95050405
rjawor <rjawor@amu.edu.pl>
2017-11-23 08:30:56 +0000

    turned off compiling of some components for re2

--- psi-pipe-or-server.sh
+++ psi-pipe-or-server.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-if [ $# -eq 0 ] \
-    || [ $1 == "--address" ] \
-    || [ $1 == "--port" ] \
-    || [ $1 == "--threads" ] \
-    || [ $1 == "--root" ] \
-    || [ $1 == "--daemon" ] \
-    || [ $1 == "--leave-standard-descriptors-when-daemonizing" ] \
-    || [ $1 == "--verbose" ]
-then
-    ./server/psi-server "$@"
-else
-    ./framework/psi-pipe "$@"
-fi
+sleep infinity
